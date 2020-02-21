be4723ad3de20f3884693bda7abf1817ea95aae1
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-02-21 10:29:13 +0800

    back

---
+++ Examples/tests/TestRead.cc
@@ -0,0 +1,22 @@
+#include <TFile.h>
+#include <TTree.h>
+#include <TROOT.h>
+
+#include <iostream>
+#include <vector>
+
+#include <Event/SimHeader.h>
+
+int TestRead()
+{
+    TFile *f=TFile::Open("$SGD/user-detsim-1000.root","READ");
+    TTree* tree_hdr = (TTree*)f->Get("Event/Sim/SimHeader");
+    TTree* tree_evt = (TTree*)f->Get("Event/Sim/SimEvent");
+    JM::SimHeader* sh7 = 0;
+    JM::SimEvent* se7 = 0;
+    tree_hdr->SetBranchAddress("SimHeader", &sh7);
+    tree_evt->SetBranchAddress("SimEvent", &se7);
+    tree_hdr->GetBranch("SimHeader")->SetAutoDelete(true);
+    tree_evt->GetBranch("SimEvent")->SetAutoDelete(true);
+
+}
