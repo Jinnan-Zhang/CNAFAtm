ed1a8cdbd75451414efbca259db6ff004653ace6
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-04-11 16:25:00 +0800

    python better

---
+++ Project/Py_AtmJUNO/AtmJUNO.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import os
+o_path = os.getcwd()
+sys.path.append(o_path)
+from lib.PreImport import *
+
+if __name__ == "__main__":
+    print(SGD)
---
+++ Project/Py_AtmJUNO/lib/PreImport.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import ROOT
+try:
+    import numpy as np
+except:
+    print("Failed to import numpy.")
+    exit()
+#detector simulation path 
+SGD="/storage/gpfs_data/juno/junofs/production/public/users/sgiulio/J18v1r1-Pre1/Atmospheric/0-20GeV/detsim/user-detsim-"
