85c98350979f6e131e2f0847f64a7373a8f1df8a
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-05-17 20:22:12 +0800

    try cpp

--- Project/Py_AtmJUNO/Cppsrc/Wphits.cc
+++ Project/Py_AtmJUNO/Cppsrc/Wphits.cc
@@ -38,6 +38,29 @@ public:
     ClassDef(SimPMTHit, 6)
 };
 ClassImp(SimPMTHit);
-extern "C" {
-    SimPMTHit obj;
+
+#define SGD1 "/storage/gpfs_data/juno/junofs/production/public/users/sgiulio/J18v1r1-Pre1/Atmospheric/0-20GeV/detsim/detsim-"
+#include <TFile.h>
+#include <TChain.h>
+
+void AddDetsimFile2TChain(TChain &tree, int NFiles = 1, int startFile = 0)
+{
+    for (int i = startFile; i < startFile + NFiles; i++)
+    {
+        tree.Add(Form("%s%d.root", SGD1, i));
+    }
+}
+int Wphit()
+{
+    TChain SimEvent("Event/Sim");
+    AddDetsimFile2TChain(SimEvent);
+    std::vector< SimPMTHit *> m_wp_hits={0};
+    TBranch *brWp=0;
+    SimEvent.SetBranchAddress("SimEvent.m_wp_hits",&m_wp_hits,&brWp);
+    SimEvent.SetCacheSize(10000000);
+    SimEvent.AddBranchToCache("SimEvent.m_wp_hits");
+    brWp->GetEntry(1);
+    printf("wpPe:%d\n",m_wp_hits[0]->getNPE());
+
+    return 0;
 }
