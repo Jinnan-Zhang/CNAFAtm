2ea35bb7082d84e7287fe0b53c9d31b5e12b1eea
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-05-17 16:56:43 +0800

    how to use SimPMTHit?

---
+++ Project/Py_AtmJUNO/Cppsrc/SimPMTHit.cc
@@ -0,0 +1,43 @@
+
+#include "TObject.h"
+#include <vector>
+#include <iostream>
+class SimPMTHit : public TObject
+{
+private:
+    int pmtid;
+    int npe;
+    double hittime;
+    double timewindow;
+    int trackid; // ref to Track ID. but if hits are merged, unknown.
+public:
+    SimPMTHit()
+    {
+        pmtid = -1;
+        npe = -1;
+        hittime = -1;
+        timewindow = 0;
+        trackid = -1;
+    }
+
+    virtual ~SimPMTHit() {}
+
+public:
+    int getPMTID() { return pmtid; }
+    int getNPE() { return npe; }
+    double getHitTime() { return hittime; }
+    double getTimeWindow() { return timewindow; }
+    int getTrackID() { return trackid; }
+
+    void setPMTID(int val) { pmtid = val; }
+    void setNPE(int val) { npe = val; }
+    void setHitTime(double val) { hittime = val; }
+    void setTimeWindow(double val) { timewindow = val; }
+    void setTrackID(int val) { trackid = val; }
+
+    ClassDef(SimPMTHit, 6)
+};
+ClassImp(SimPMTHit);
+extern "C" {
+    SimPMTHit obj;
+}
