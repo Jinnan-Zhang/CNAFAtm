997fe328860c4f58478f46d19485b7adbb0afdc7
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-05-17 23:13:32 +0800

    on trying

--- Project/Py_AtmJUNO/Cppsrc/Wphits.cc
+++ Project/Py_AtmJUNO/Cppsrc/Wphits.cc
@@ -2,49 +2,11 @@
 #include "TObject.h"
 #include <vector>
 #include <iostream>
-namespace JM
-{
-    class SimPMTHit : public TObject
-    {
-    private:
-        int pmtid;
-        int npe;
-        double hittime;
-        double timewindow;
-        int trackid; // ref to Track ID. but if hits are merged, unknown.
-    public:
-        SimPMTHit()
-        {
-            pmtid = -1;
-            npe = -1;
-            hittime = -1;
-            timewindow = 0;
-            trackid = -1;
-        }
-
-        virtual ~SimPMTHit() {}
-
-    public:
-        int getPMTID() { return pmtid; }
-        int getNPE() { return npe; }
-        double getHitTime() { return hittime; }
-        double getTimeWindow() { return timewindow; }
-        int getTrackID() { return trackid; }
-
-        void setPMTID(int val) { pmtid = val; }
-        void setNPE(int val) { npe = val; }
-        void setHitTime(double val) { hittime = val; }
-        void setTimeWindow(double val) { timewindow = val; }
-        void setTrackID(int val) { trackid = val; }
-
-        ClassDef(SimPMTHit, 6)
-    };
-} // namespace JM
-ClassImp(JM::SimPMTHit);
-
-#define SGD1 "/storage/gpfs_data/juno/junofs/production/public/users/sgiulio/J18v1r1-Pre1/Atmospheric/0-20GeV/detsim/detsim-"
 #include <TFile.h>
 #include <TChain.h>
+#define SGD1 "/storage/gpfs_data/juno/junofs/production/public/users/sgiulio/J18v1r1-Pre1/Atmospheric/0-20GeV/detsim/detsim-"
+#include "Event/SimPMTHit.h"
+
 
 void AddDetsimFile2TChain(TChain &tree, int NFiles = 1, int startFile = 1)
 {
