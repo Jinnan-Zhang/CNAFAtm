287ea12e3b85691f5bf8eb272336ec5b1774bc91
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-05-17 13:24:41 +0800

    file range

--- Project/Py_AtmJUNO/lib/PreImport.py
+++ Project/Py_AtmJUNO/lib/PreImport.py
@@ -10,6 +10,5 @@ except:
 SGD="/storage/gpfs_data/juno/junofs/production/public/users/sgiulio/J18v1r1-Pre1/Atmospheric/0-20GeV/detsim/user-detsim-"
 
 def AddFile2TChain(tree, NFiles=1):
-    for nn in range(10000, 10000 + NFiles):
-        if nn != 10216:
-            tree.Add(SGD + str(nn) + ".root")
+    for nn in range(1,1+NFiles):
+        tree.Add(SGD + str(nn) + ".root")
