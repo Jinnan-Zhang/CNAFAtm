4552e1a16cb4d2dcc67d2e73bbf7cfbed6497382
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-05-17 19:11:00 +0800

    FCN?

--- Project/Py_AtmJUNO/src/GetTimeResiual.py
+++ Project/Py_AtmJUNO/src/GetTimeResiual.py
@@ -48,7 +48,7 @@ def ViewWaterPoolPEs(NFiles, WhichEntry=0, SaveFileName="WPnpe"):
     SimEvent = ROOT.TChain("/Event/Sim/SimEvent")
     AddDetsimFile2TChain(SimEvent, NFiles=NFiles)
     c = ROOT.TCanvas("myCanvasName", "The Canvas Title", 800, 600)
-    SimEvent.Draw("SimEvent.m_wp_hits.npe>>+h_wp_npe")
+    SimEvent.Draw("SimEvent.m_wp_hits.getNPE()>>+h_wp_npe")
     ROOT.gStyle.SetOptStat("ne")
     c.SaveAs("./pics/"+SaveFileName + ".png")
 
