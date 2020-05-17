03b29cacb7fca024b896fd470afafdd55f137c64
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-05-17 12:47:38 +0800

    try global pos

--- Project/Py_AtmJUNO/AtmJUNO.py
+++ Project/Py_AtmJUNO/AtmJUNO.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from src.GetTimeResiual import *
+from lib.PreImport import *
 import sys
 import os
 o_path = os.getcwd()
 sys.path.append(o_path)
-from lib.PreImport import *
 
 if __name__ == "__main__":
-    print(SGD)
+    # print(SGD)
+    ViewGlobalPos(NFiles=int(sys.argv[1]),WhichEntry=int(sys.argv[2]))
---
+++ Project/Py_AtmJUNO/src/GetTimeResiual.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from lib.PreImport import *
+import sys
+import os
+sys.path.append("..")
+
+
+def ViewGlobalPos(NFiles, WhichEntry=0, SaveFileName="GlobalPos"):
+    ROOT.ROOT.EnableImplicitMT()
+    evt = ROOT.TChain("evt")
+    AddFile2TChain(evt, NFiles=NFiles)
+    c = ROOT.TCanvas("myCanvasName", "The Canvas Title", 800, 600)
+    evt.Draw(
+        "sqrt(GlobalPosX*GlobalPosX+GlobalPosY*GlobalPosY+GlobalPosZ*GlobalPosZ)/1000>>+h_GPos",
+        "pmtID>150e3",
+        "", 1, WhichEntry)
+    h_GPos = ROOT.gDirectory.Get("h_GPos")
+    h_GPos.SetXTitle("m")
+    ROOT.gStyle.SetOptStat("ne")
+    c.SaveAs(SaveFileName + ".png")
+
+
+if __name__ == "__main__":
+    pass
