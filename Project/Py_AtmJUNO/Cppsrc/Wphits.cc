ae3dda99f3ff4ea373c998035100b000648d3be3
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-05-18 08:49:08 +0800

    with headers?

--- Project/Py_AtmJUNO/Cppsrc/Wphits.cc
+++ Project/Py_AtmJUNO/Cppsrc/Wphits.cc
@@ -4,10 +4,12 @@
 #include <iostream>
 #include <TFile.h>
 #include <TChain.h>
-#define SGD1 "/storage/gpfs_data/juno/junofs/production/public/users/sgiulio/J18v1r1-Pre1/Atmospheric/0-20GeV/detsim/detsim-"
 #include "Event/SimPMTHit.h"
 #include "Event/SimEvent.h"
 #include "Event/SimHeader.h"
+#define SGD1 "/storage/gpfs_data/juno/junofs/production/public/users/sgiulio/J18v1r1-Pre1/Atmospheric/0-20GeV/detsim/detsim-"
+#define HXD "/junofs/users/huangx/production/J19v1r0-Pre3bk/IBD/uniform/IBD/detsim/user-detsim-"
+#define HXD1 "/junofs/users/huangx/production/J19v1r0-Pre3bk/IBD/uniform/IBD/detsim/detsim-"
 
 
 void AddDetsimFile2TChain(TChain &tree, int NFiles = 1, int startFile = 1)
@@ -15,13 +17,14 @@ void AddDetsimFile2TChain(TChain &tree, int NFiles = 1, int startFile = 1)
     for (int i = startFile; i < startFile + NFiles; i++)
     {
         tree.Add(Form("%s%d.root", SGD1, i));
+        // tree.Add(Form("%s%d.root", HXD1, i));
     }
 }
 int Wphits()
 {
     TChain SimEvent1("Event/Sim/SimEvent");
     AddDetsimFile2TChain(SimEvent1);
-    JM::SimHeader* sh7 = 0;
+    // JM::SimHeader* sh7 = 0;
     JM::SimEvent* se7 = 0;
     SimEvent1.SetBranchAddress("SimEvent",&se7);
     SimEvent1.GetBranch("SimEvent")->SetAutoDelete(true);
@@ -35,6 +38,6 @@ int Wphits()
             JM::SimPMTHit* hit = *it;
             totalpe += hit->getNPE();
         }
-
+    printf("totalPE:%d\n",totalpe);
     return 0;
 }
