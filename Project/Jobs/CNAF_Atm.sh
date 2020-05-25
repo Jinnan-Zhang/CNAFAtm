f0346bcaf8cc0a50f7304e4913be79e767cd2101
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-05-25 23:33:34 +0800

    try jobs

--- Project/Jobs/CNAF_Atm.sh
+++ Project/Jobs/CNAF_Atm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /storage/gpfs_data/juno/junofs/users/zhangjinnan/Tools/ROOT/6.20.04/bin/thisroot.sh
 cd /storage/gpfs_data/juno/junofs/users/zhangjinnan/Atm/CNAF_Atm/Project/Py_AtmJUNO
-# python AtmJUNO.py 50 START
-python AtmJUNO.py 3 1
+python AtmJUNO.py 50 START
+# python AtmJUNO.py 3 1
--- Project/Jobs/Ge_CNAF_jobs.sh
+++ Project/Jobs/Ge_CNAF_jobs.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-for((Target=0;Target<4;Target++))
+for((Target=1;Target<100;Target+=50))
 do
     echo dealing...Files begin from:$Target 
     sed "s/START/$Target/g"  $1.sh > $1$Target.sh
     chmod 774 $1$Target.sh
-    
-
-    # hep_sub -mem 4000 $1$"%{ProcId}".sh -n 4 
+    bsub -q juno -o ${PWD}/logs/Sel_$Target.log -hl -M 4000000 ${PWD}/$1$Target.sh
 done
-
+# bsub -q juno -o myo3.log ${PWD}/CNAF_Atm.sh
 
 #end
