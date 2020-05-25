50567dd7a8990df9281f04a7eb194ac1b5a3e39e
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-05-25 20:41:33 +0800

    main

--- Project/Py_AtmJUNO/AtmJUNO.py
+++ Project/Py_AtmJUNO/AtmJUNO.py
@@ -10,9 +10,9 @@ sys.path.append(o_path)
 if __name__ == "__main__":
     # print(SGD)
     # ViewGlobalPos(NFiles=int(sys.argv[1]),WhichEntry=int(sys.argv[2]))
-    ViewHitTime(NFiles=int(sys.argv[1]),WhichEntry=int(sys.argv[2]))
+    # ViewHitTime(NFiles=int(sys.argv[1]),WhichEntry=int(sys.argv[2]))
     # ViewPDGID(NFiles=int(sys.argv[1]),WhichEntry=int(sys.argv[2]))
     # ViewWaterPoolPEs(NFiles=int(sys.argv[1]), WhichEntry=int(sys.argv[2]))
     # ViewPMTID(NFiles=int(sys.argv[1]), WhichEntry=int(sys.argv[2]))
     
-    # ViewTimeProfile(NFiles=int(sys.argv[1]),StartFile=int(sys.argv[2]))
+    ViewTimeProfile(NFiles=int(sys.argv[1]),StartFile=int(sys.argv[2]))
