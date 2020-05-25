87b8f7021bfea4396fea5619524751b1ded048b0
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-05-25 17:07:10 +0200

    3

--- Project/Jobs/CNAF_Atm.sh
+++ Project/Jobs/CNAF_Atm.sh
@@ -2,4 +2,4 @@
 source /storage/gpfs_data/juno/junofs/users/zhangjinnan/Tools/ROOT/6.20.04/bin/thisroot.sh
 cd /storage/gpfs_data/juno/junofs/users/zhangjinnan/Atm/CNAF_Atm/Project/Py_AtmJUNO
 # python AtmJUNO.py 50 START
-python AtmJUNO.py 2 1
+python AtmJUNO.py 3 1
