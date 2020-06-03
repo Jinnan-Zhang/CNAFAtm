0a12afc74f36c4419421e668abab7732b561380e
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-06-03 09:11:00 +0800

    try again

--- Project/Jobs/Ge_CNAF_jobs.sh
+++ Project/Jobs/Ge_CNAF_jobs.sh
@@ -5,8 +5,8 @@ do
     echo dealing...Files begin from:$Target 
     sed "s/START/$Target/g"  $1.sh > $1$Target.sh
     chmod 774 $1$Target.sh
-    # bsub -q juno -o ${PWD}/logs/Sel_$Target.log -hl -M 4000000 ${PWD}/$1$Target.sh
-    echo ${PWD}/$1$Target.sh
+    bsub -q juno -o ${PWD}/logs/Sel_$Target.log -hl -M 4000000 ${PWD}/$1$Target.sh
+    # echo ${PWD}/$1$Target.sh
 done
 # bsub -q juno -o myo3.log ${PWD}/CNAF_Atm.sh
 
