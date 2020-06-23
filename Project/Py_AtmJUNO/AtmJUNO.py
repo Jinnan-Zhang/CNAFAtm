59848bcf70f82451f0613de132dea5d5ccd89465
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-06-23 08:20:41 +0800

    on trees

--- Project/Py_AtmJUNO/AtmJUNO.py
+++ Project/Py_AtmJUNO/AtmJUNO.py
@@ -15,15 +15,15 @@ if __name__ == "__main__":
     # ViewPDGID(NFiles=int(sys.argv[1]),WhichEntry=int(sys.argv[2]))
     # ViewWaterPoolPEs(NFiles=int(sys.argv[1]), WhichEntry=int(sys.argv[2]))
     # ViewPMTID(NFiles=int(sys.argv[1]), WhichEntry=int(sys.argv[2]))
-    if(sys.argv[1] == "TPro"):
+    if (sys.argv[1] == "TPro"):
         if (len(sys.argv) == 4):
-            ViewTimeProfile(NFiles=int(sys.argv[1+1]),
-                            StartFile=int(sys.argv[2+1]),
-                            SaveFileName=sys.argv[3+1])
+            ViewTimeProfile(NFiles=int(sys.argv[1 + 1]),
+                            StartFile=int(sys.argv[2 + 1]),
+                            SaveFileName=sys.argv[3 + 1])
         else:
             ViewTimeProfile(NFiles=int(sys.argv[1]),
                             StartFile=int(sys.argv[2]))
-    elif(sys.argv[1] == "NPETE"):  # all information
-        GetNPE_Tres_Energy_Profile(NFiles=1)
-    elif(sys.argv[1] == "NcapT"):  # view nCatureTime
+    elif (sys.argv[1] == "NcapT"):  # view nCatureTime
         ViewnCaptureT(int(sys.argv[2]))
+    elif (sys.argv[1] == "NPETE"):  # all information
+        GetNPE_Tres_Energy_Profile(NFiles=1)
