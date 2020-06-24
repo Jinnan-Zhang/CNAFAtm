052037c64563f167723fe7809a803fc8e3b468c0
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-06-24 19:17:05 +0800

    seems ok

--- Project/Jobs/CondorSub/GeSub.sh
+++ Project/Jobs/CondorSub/GeSub.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-condor_submit -name sn-01.cr.cnaf.infn.it -spool NPETE.sub
+for((Target=1;Target<5101;Target+=100))
+do
+    echo dealing...Files begin from:$Target 
+    sed "s/START/$Target/g"  $1.sh > $1$Target.sh
+    sed "s/NPETE/$1$Target/g" $1.sub > $1$Target.sub
+    chmod 774 $1$Target.sh
+    condor_submit -name sn-01.cr.cnaf.infn.it -spool $1$Target.sub
+    # bsub -q juno -o ${PWD}/logs/NEPTE_$Target.log -hl -M 4000000 ${PWD}/$1$Target.sh
+    echo ${PWD}/$1$Target.sh
+done
+#end
