205b2ddda61d85c2049d15ebc4b5bcca688d92cd
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-06-25 00:03:10 +0800

    !=

--- Project/Jobs/CondorSub/ReSub.sh
+++ Project/Jobs/CondorSub/ReSub.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 NOreSub=(2901 3101 4001 1401 3901 4201 4401 1801 301 4101 )
-for((Target=1;Target<5101;Target+=100))
+for((Target=1;Target<510;Target+=100))
 do
-    echo dealing...Files begin from:$Target 
-    # sed "s/START/$Target/g"  $1.sh > $1$Target.sh
-    # sed "s/NPETE/$1$Target/g" $1.sub > $1$Target.sub
-    chmod 774 $1$Target.sh
-    # condor_submit -name sn-01.cr.cnaf.infn.it -spool $1$Target.sub
-    # bsub -q juno -o ${PWD}/logs/NEPTE_$Target.log -hl -M 4000000 ${PWD}/$1$Target.sh
-    echo ${PWD}/$1$Target.sh
+    if [[ "${NOreSub[@]}" =~ $Target ]]
+    then
+        echo dealing...Files begin from:$Target 
+        # sed "s/START/$Target/g"  $1.sh > $1$Target.sh
+        # sed "s/NPETE/$1$Target/g" $1.sub > $1$Target.sub
+        # chmod 774 $1$Target.sh
+        # condor_submit -name sn-01.cr.cnaf.infn.it -spool $1$Target.sub
+        # echo ${PWD}/$1$Target.sh
+    fi
 done
 #end
