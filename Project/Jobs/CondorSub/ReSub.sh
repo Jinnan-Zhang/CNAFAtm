48584b3f788bbd51b4f1c3e3190d276f82844ebe
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-06-25 09:06:03 +0800

    resub ok

--- Project/Jobs/CondorSub/ReSub.sh
+++ Project/Jobs/CondorSub/ReSub.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-NOreSub=(2901 3101 4001 1401 3901 4201 4401 1801 301 4101 )
+NOreSub=( 2901 3101 4001 1401 3901 4201 4401 1801 301 4101 )
 for((Target=1;Target<510;Target+=100))
 do
-    if [[ "${NOreSub[@]}" =~ $Target ]]
+    COUNT_re=0
+    for re in ${NOreSub[@]}
+    do
+        if [ $re -eq $Target ]
+        then
+            COUNT_re=`expr $COUNT_re + 1`
+            break
+        fi
+    done
+    if [ $COUNT_re -eq 0 ]
     then
         echo dealing...Files begin from:$Target 
-        # sed "s/START/$Target/g"  $1.sh > $1$Target.sh
-        # sed "s/NPETE/$1$Target/g" $1.sub > $1$Target.sub
-        # chmod 774 $1$Target.sh
-        # condor_submit -name sn-01.cr.cnaf.infn.it -spool $1$Target.sub
+        sed "s/START/$Target/g"  $1.sh > $1$Target.sh
+        sed "s/NPETE/$1$Target/g" $1.sub > $1$Target.sub
+        chmod 774 $1$Target.sh
+        condor_submit -name sn-01.cr.cnaf.infn.it -spool $1$Target.sub
         # echo ${PWD}/$1$Target.sh
     fi
 done
